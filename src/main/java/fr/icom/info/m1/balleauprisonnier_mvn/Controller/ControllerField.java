56b70c066ec36aa36f61dc01c4c768feda364f1d
Feriel Chamakh <fchamakh@univ-lyon2.fr>
2022-10-24 15:58:24 +0200

    mvc

--- src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Controller/ControllerField.java
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Controller/ControllerField.java
@@ -1,5 +1,13 @@
 package fr.icom.info.m1.balleauprisonnier_mvn.Controller;
 
-public class ControllerField {
+import java.util.ArrayList;
 
+import fr.icom.info.m1.balleauprisonnier_mvn.Player;
+
+public class ControllerField {
+	
+	 ArrayList<String> input = new ArrayList<String>();
+	 public Player[] getJoueurs() {
+			return joueurs;
+		}
 }
--- src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Model/ModelPlayer.java
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Model/ModelPlayer.java
@@ -1,5 +1,10 @@
 package fr.icom.info.m1.balleauprisonnier_mvn.Model;
 
 public class ModelPlayer {
+	double x;       // position horizontale du joueur
+	  final double y; 	  // position verticale du joueur
+	  double angle = 90; // rotation du joueur, devrait toujour être en 0 et 180
+	  double step;    // pas d'un joueur
+	
 
 }
